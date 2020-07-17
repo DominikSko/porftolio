e1dc5d3998497568a657408b3e3995c44009025e
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-07-17 02:01:59 +0200

    heroku deploy

---
+++ build/precache-manifest.5e019b9391a80ab314bc5d2382bf2e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5e4ac8122abab191c5a71cb38b960e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70080c0d48060f9ab0da",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "cc36b045786f297c6eea",
+    "url": "/static/js/2.c08e30cc.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.c08e30cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70080c0d48060f9ab0da",
+    "url": "/static/js/main.fcc9cb34.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  }
+]);
